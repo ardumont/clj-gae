057517456e05f0e48ccfd31711129523110a42a7
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2013-06-01 21:31:02 +0200

    Improve the script to deploy.

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,15 @@
 #!/bin/bash -xe
 
+# cleanup
 lein clean
 
+# workaround to deploy all lib in classpath
+lein uberjar
+mkdir -p war/WEB-INF/lib
+mv target/clj-gae-1.0.0-SNAPSHOT-standalone.jar war/WEB-INF/lib/
+
+# aot compile clj in war/WEB-INF/classes
 lein compile
 
+# upload war in appengine
 appcfg.sh update war
