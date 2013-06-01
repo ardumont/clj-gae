a14fa8430e2529870eea96583b4ff06df47e27df
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2013-06-01 20:02:21 +0200

    A simple /hello rest call.

--- src/clj_gae/core.clj
+++ src/clj_gae/core.clj
@@ -1,21 +1,18 @@
 (ns clj-gae.core
   (:gen-class :extends javax.servlet.http.HttpServlet)
-  (:use compojure.core
+  (:use [compojure.core]
         [ring.util.servlet   :only [defservice]]
-        [ring.util.response  :only [redirect]]
-        [hiccup.core         :only [h html]]
-        [hiccup.page-helpers :only [doctype include-css link-to xhtml-tag]]
-        [hiccup.form-helpers :only [form-to text-area text-field]])
+        [ring.util.response  :only [redirect]])
   (:import (com.google.appengine.api.datastore Query))
   (:require [compojure.route          :as route]
             [appengine.datastore.core :as ds]
             [appengine.users          :as users]))
+(comment
+  (defroutes public-routes
+    (GET "/hello" [] {:status 200
+                      :headers {"Content-Type" "text/plain"}
+                      :body (str "Hello clj on AppEngine!"
+                                 "\n\n"
+                                 " [powered by AppEngine and clojure]")}))
 
-(defroutes public-routes
-  (GET "/" [] {:status 200
-               :headers {"Content-Type" "text/plain"}
-               :body (str "Hello clj on AppEngine!"
-                          "\n\n"
-                          " [powered by AppEngine and clojure]")}))
-
-(defservice public-routes)
+  (defservice public-routes))
